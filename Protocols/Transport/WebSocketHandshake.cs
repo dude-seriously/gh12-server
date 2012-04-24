c682645e02b616004efed5f6b906c6458ed463a9
Maksims Mihejevs <mokadwod@gmail.com>
2012-04-24 16:38:06 +0200

    indention

--- Protocols/Transport/WebSocketHandshake.cs
+++ Protocols/Transport/WebSocketHandshake.cs
@@ -6,143 +6,137 @@ using System.Linq;
 using System.Reflection;
 
 namespace Server.Protocols.Transport {
-	sealed class WSHandshakeParser {
-		private string data;
-		private uint version;
-		private HandshakeData info;
+    sealed class WSHandshakeParser {
+        private string data;
+        private uint version;
+        private HandshakeData info;
 
-		public WSHandshakeParser(byte[] buffer) {
-			//this.data = Encoding.UTF8.GetString(buffer);
+        public WSHandshakeParser(byte[] buffer) {
+            this.version = WSHandhsakeVersions.GetVersion(ref buffer);
 
-			//Log.Add(Encoding.UTF8.GetString(buffer));
+            IWSHandshake parser = WSHandhsakeVersions.Get(this.version);
+            if(parser != null) {
+                this.info = parser.Parse(ref buffer);
+            } else {
+                Log.Add("handshake data: " + this.data);
+            }
+        }
 
-			this.version = WSHandhsakeVersions.GetVersion(ref buffer);
+        public uint Version {
+            get { return this.version; }
+        }
 
-			//Log.Add(this.version.ToString());
+        public HandshakeData Info {
+            get { return this.info; }
+        }
+    }
 
-			IWSHandshake parser = WSHandhsakeVersions.Get(this.version);
-			if(parser != null) {
-				this.info = parser.Parse(ref buffer);
-			} else {
-				Log.Add("handshake data: " + this.data);
-			}
-		}
+    sealed class WSHandhsakeVersions {
+        static private volatile WSHandhsakeVersions instance = new WSHandhsakeVersions();
+        private Dictionary<uint, IWSHandshake> container;
 
-		public uint Version {
-			get { return this.version; }
-		}
+        private WSHandhsakeVersions() {
+            this.container = new Dictionary<uint, IWSHandshake>();
 
-		public HandshakeData Info {
-			get { return this.info; }
-		}
-	}
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            IEnumerable<Type> types = assembly.GetTypes().Where(c => c.GetInterface(typeof(IWSHandshake).Name) != null && c.GetCustomAttributes(typeof(AWSHandshake), false).Length == 1);
+            foreach(Type type in types) {
+                AWSHandshake attribute = (AWSHandshake)(type.GetCustomAttributes(typeof(AWSHandshake), false)[0]);
+                foreach(uint version in attribute.Versions) {
+                    FieldInfo instanceField = type.GetField("instance", BindingFlags.NonPublic | BindingFlags.Static);
+                    if(instanceField != null) {
+                        container.Add(version, (IWSHandshake)instanceField.GetValue(null));
+                    }
+                }
+            }
+        }
 
-	sealed class WSHandhsakeVersions {
-		static private volatile WSHandhsakeVersions instance = new WSHandhsakeVersions();
-		private Dictionary<uint, IWSHandshake> container;
+        static public uint GetVersion(ref byte[] data) {
+            string dataString = Encoding.UTF8.GetString(data);
+            Match result = Regex.Match(dataString, @"^Sec-WebSocket-Version: (?<version>\d+)\s?$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
 
-		private WSHandhsakeVersions() {
-			this.container = new Dictionary<uint, IWSHandshake>();
+            if(result.Success && result.Groups["version"] != null) {
+                uint version = 0;
+                uint.TryParse(result.Groups["version"].Value, out version);
 
-			Assembly assembly = Assembly.GetExecutingAssembly();
-			IEnumerable<Type> types = assembly.GetTypes().Where(c => c.GetInterface(typeof(IWSHandshake).Name) != null && c.GetCustomAttributes(typeof(AWSHandshake), false).Length == 1);
-			foreach(Type type in types) {
-				AWSHandshake attribute = (AWSHandshake)(type.GetCustomAttributes(typeof(AWSHandshake), false)[0]);
-				foreach(uint version in attribute.Versions) {
-					FieldInfo instanceField = type.GetField("instance", BindingFlags.NonPublic | BindingFlags.Static);
-					if(instanceField != null) {
-						container.Add(version, (IWSHandshake)instanceField.GetValue(null));
-					}
-				}
-			}
-		}
+                return version;
+            } else {
+                return 0;
+            }
+        }
 
-		static public uint GetVersion(ref byte[] data) {
-			string dataString = Encoding.UTF8.GetString(data);
-			Match result = Regex.Match(dataString, @"^Sec-WebSocket-Version: (?<version>\d+)\s?$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
+        static public string GetValue(ref string data, string name) {
+            Match result = Regex.Match(data, @"^" + name + @": (?<value>[\da-zA-Z :/\-+=*();.,_'<]+)\s?$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
 
-			if(result.Success && result.Groups["version"] != null) {
-				uint version = 0;
-				uint.TryParse(result.Groups["version"].Value, out version);
+            if(result.Success && result.Groups["value"] != null) {
+                return result.Groups["value"].Value;
+            } else {
+                return string.Empty;
+            }
+        }
 
-				return version;
-			} else {
-				return 0;
-			}
-		}
+        static public string GetKeyValue(ref string data, string name) {
+            Match result = Regex.Match(data, @"^" + name + @": (?<value>.+)\r$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
 
-		static public string GetValue(ref string data, string name) {
-			Match result = Regex.Match(data, @"^" + name + @": (?<value>[\da-zA-Z :/\-+=*();.,_'<]+)\s?$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
+            if(result.Success && result.Groups["value"] != null) {
+                return result.Groups["value"].Value;
+            } else {
+                return string.Empty;
+            }
+        }
 
-			if(result.Success && result.Groups["value"] != null) {
-				return result.Groups["value"].Value;
-			} else {
-				return string.Empty;
-			}
-		}
+        static public IWSHandshake Get(uint version) {
+            if(instance.container.ContainsKey(version)) {
+                return instance.container[version];
+            } else {
+                return null;
+            }
+        }
+    }
 
-		static public string GetKeyValue(ref string data, string name) {
-			Match result = Regex.Match(data, @"^" + name + @": (?<value>.+)\r$", RegexOptions.ExplicitCapture | RegexOptions.Multiline);
+    sealed class AWSHandshake : Attribute {
+        private uint[] versions;
 
-			if(result.Success && result.Groups["value"] != null) {
-				return result.Groups["value"].Value;
-			} else {
-				return string.Empty;
-			}
-		}
+        public AWSHandshake(uint version) {
+            this.versions = new uint[] { version };
+        }
 
-		static public IWSHandshake Get(uint version) {
-			if(instance.container.ContainsKey(version)) {
-				return instance.container[version];
-			} else {
-				return null;
-			}
-		}
-	}
+        public AWSHandshake(params uint[] versions) {
+            this.versions = versions;
+        }
 
-	sealed class AWSHandshake : Attribute {
-		private uint[] versions;
+        public uint[] Versions {
+            get { return this.versions; }
+        }
+    }
 
-		public AWSHandshake(uint version) {
-			this.versions = new uint[] { version };
-		}
+    interface IWSHandshake {
+        HandshakeData Parse(ref byte[] data);
+    }
 
-		public AWSHandshake(params uint[] versions) {
-			this.versions = versions;
-		}
+    sealed class HandshakeData {
+        private Dictionary<string, string> container;
+        private string response;
 
-		public uint[] Versions {
-			get { return this.versions; }
-		}
-	}
+        public HandshakeData() {
+            this.container = new Dictionary<string, string>();
+        }
 
-	interface IWSHandshake {
-		HandshakeData Parse(ref byte[] data);
-	}
+        public void Add(string name, string value) {
+            this.container.Add(name, value);
+        }
 
-	sealed class HandshakeData {
-		private Dictionary<string, string> container;
-		private string response;
+        public string Get(string name) {
+            if(this.container.ContainsKey(name)) {
+                return this.container[name];
+            } else {
+                return string.Empty;
+            }
+        }
 
-		public HandshakeData() {
-			this.container = new Dictionary<string, string>();
-		}
-
-		public void Add(string name, string value) {
-			this.container.Add(name, value);
-		}
-
-		public string Get(string name) {
-			if(this.container.ContainsKey(name)) {
-				return this.container[name];
-			} else {
-				return string.Empty;
-			}
-		}
-
-		public string Response {
-			set { this.response = value; }
-			get { return this.response; }
-		}
-	}
+        public string Response {
+            set { this.response = value; }
+            get { return this.response; }
+        }
+    }
 }
